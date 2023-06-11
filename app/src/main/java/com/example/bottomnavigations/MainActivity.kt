2f590c4bf3207e0f6e3ae2cb2d4a2a17a2b9e310
Hirad8520 <amirrezaj37@gmail.com>
2023-06-11 12:36:50 +0330

    ma

--- app/src/main/java/com/example/bottomnavigations/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigations/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            bottomnavigations {
+            BottomnavigationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
