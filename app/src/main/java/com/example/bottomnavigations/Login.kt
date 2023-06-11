b7d83aac42dce9ccddc33ca4d656dcbeae64e2ab
Hirad8520 <amirrezaj37@gmail.com>
2023-06-11 12:30:11 +0330

    fo

---
+++ app/src/main/java/com/example/bottomnavigations/Login.kt
@@ -0,0 +1,18 @@
+package com.example.bottomnavigations
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun login(navController: NavHostController) {
+    Column() {
+        Text(text = "login")
+        Button(onClick = { navController.navigate(Welcome.Route) }) {
+            Text(text = "Login")
+        }
+    }
+}
